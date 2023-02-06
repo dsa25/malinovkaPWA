d6392da2baea67f025031cf2ee6242d83b60610e
dsa25 <73094994+dsa25@users.noreply.github.com>
2023-02-06 15:52:29 +0500

    Updates

--- sw.js
+++ sw.js
@@ -1,8 +1,31 @@
-console.log("swJs..")
-self.addEventListener("install", (event) => {
+console.log("sw.js:::")
+
+const CACHE_VERSION = "v1"
+
+const CACHE_FILES = [
+  "index.html",
+  "vite.svg",
+  "/assets/index-25b2ad5e.js",
+  "/assets/index-a76659ee.css",
+  "/assets/sprite-b3562787.svg"
+]
+
+self.addEventListener("install", async (event) => {
+  const cache = await caches.open(CACHE_VERSION)
+  await cache.addAll(CACHE_FILES)
   console.log("sw.install")
 })
 
 self.addEventListener("activate", (event) => {
   console.log("sw.activate")
 })
+
+self.addEventListener("fetch", (event) => {
+  console.log("FetchV1", event.request.url)
+  event.respondWith(cacheFirst(event.request))
+})
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request)
+  return cached ?? (await fetch(request))
+}
