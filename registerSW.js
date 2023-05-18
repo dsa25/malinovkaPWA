38f9622c9458e282c142464c206d92dc106b779a
dsa25 <73094994+dsa25@users.noreply.github.com>
2023-05-18 16:23:22 +0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/malinovkaPWA/sw.js', { scope: '/malinovkaPWA/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
